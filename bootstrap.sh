0dd7e0b55ac74f29fda49944198a9a2aef7b8392
mahatmus-tech <aranpasta@gmail.com>
2025-04-07 12:22:29 -0300

    python

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@ fi
 
 # Ensure git  is installed in the live environment
 echo "📦 Installing git in the live ISO..."
-pacman -Sy --noconfirm git archinstall
+pacman -Sy --noconfirm git archinstall python
 
 # Clone the installation repository
 echo "📥 Cloning installation repository..."
