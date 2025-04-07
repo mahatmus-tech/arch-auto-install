fb6150fd2f45458ce37c30afe14b189cebe0461f
mahatmus-tech <aranpasta@gmail.com>
2025-04-07 13:25:33 -0300

    vm config

--- archinstall/install.sh
+++ archinstall/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-archinstall --config user_configuration.json --creds user_credentials.json
+archinstall --config user_configuration_vm.json --creds user_credentials.json
 
