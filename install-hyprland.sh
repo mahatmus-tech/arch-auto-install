053baa4f310117b97d3618534e0115907f885579
mahatmus <131696843+mahatmus-tech@users.noreply.github.com>
2025-04-16 19:32:23 -0300

    Update install-hyprland.sh

--- install-hyprland.sh
+++ install-hyprland.sh
@@ -147,111 +147,36 @@ configure_hyprland() {
 		echo "windowrulev2 = norounding, tag:games*" >> "$CONFIG"
 		echo "windowrulev2 = allowsinput, tag:games*" >> "$CONFIG"
 		echo "windowrulev2 = immediate, tag:games*" >> "$CONFIG"
-
+		
 		local CONFIG="$HOME/.config/hypr/UserConfigs/UserSettings.conf"
-        sudo sed -i -E "s|^#accel_profile =.*|#accel_profile = flat|" $CONFIG
+		sudo sed -i -E "s|^#accel_profile =.*|#accel_profile = flat|" $CONFIG
 		sudo sed -i -E "s|^direct_scanout = 0.*|direct_scanout = 2|" $CONFIG
-  		sudo sed -i -E "s|^#opengl {.*|opengl {|" $CONFIG
+		sudo sed -i -E "s|^#opengl {.*|opengl {|" $CONFIG
 		sudo sed -i -E "s|^#  nvidia_anti_flicker = true.*|  nvidia_anti_flicker = true|" $CONFIG	
-  		sudo sed -i -E "s|^#}.*|}|" $CONFIG	
-	 
- 
+		sudo sed -i -E "s|^#}.*|}|" $CONFIG
+		
+		local CONFIG="$HOME/.config/hypr/UserConfigs/ENVariables.conf"
+		echo "env = GBM_BACKEND,nvidia-drm" >> "$CONFIG"
+			echo "env = __GLX_VENDOR_LIBRARY_NAME,nvidia" >> "$CONFIG"
+		echo "env = LIBVA_DRIVER_NAME,nvidia" >> "$CONFIG" 
  	else
 		# Path to Hyprland config file
 		local CONFIG="$HOME/.config/hypr/hyprland.conf"
-
+		
 		# Startup
 		exec-once = dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 		exec-once = systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP  
-        echo "exec-once = waybar" >> "$CONFIG"
+		echo "exec-once = waybar" >> "$CONFIG"
 		echo "exec-once = swaync" >> "$CONFIG"
-  		echo "exec-once = blueman-applet" >> "$CONFIG"
+		echo "exec-once = blueman-applet" >> "$CONFIG"
 		#clipboard manager
-  		echo "exec-once = wl-paste --type text --watch cliphist store" >> "$CONFIG"
+		echo "exec-once = wl-paste --type text --watch cliphist store" >> "$CONFIG"
 		echo "exec-once = wl-paste --type image --watch cliphist store" >> "$CONFIG"		
 		# Starting hypridle to start hyprlock
-  		echo "exec-once = hypridle" >> "$CONFIG"
-        # gamemoded
+		echo "exec-once = hypridle" >> "$CONFIG"
+		# gamemoded
 		echo "exec-once = systemctl --user start gamemoded.service" >> "$CONFIG"
- 	fi
-
-	
-
-	
-	# Check if line already exists
-	if grep -q "exec-once = waybar" "$CONFIG"; then
-	echo "Waybar is already configured"
-	exit 0
-	fi
-	
-	# Append the line to the end of file
-	echo "exec-once = waybar" >> "$CONFIG"
-	echo "Added Waybar to hyprland.conf"
-
-        
-    if [ "$JAYKOOLIT_INSTALLED" = true ]; then
-		# Download gamemode.ini
-		sudo rm -f /etc/gamemode.ini
-		sudo wget -P /etc https://raw.githubusercontent.com/mahatmus-tech/arch-auto-install/refs/heads/main/files/gamemode.ini
-		# Cooler Master MM720 mouse fix
-		sudo rm -f /etc/udev/rules.d/99-mm720-power.rules
-		sudo wget -P /etc/udev/rules.d https://raw.githubusercontent.com/mahatmus-tech/arch-auto-install/refs/heads/main/files/99-mm720-power.rules
-		# start
-		sudo systemctl enable --now gamemoded.service
-    fi
-    
-    if [ "$NVIDIA_INSTALLED" = true ]; then
-	    # nvidia.conf 
-		sudo rm -f /etc/modprobe.d/nvidia.conf
-	    sudo wget -P /etc/modprobe.d https://raw.githubusercontent.com/mahatmus-tech/arch-auto-install/refs/heads/main/files/nvidia.conf
-		# nvidia.rules
-		sudo rm -f /etc/udev/rules.d/89-nvidia-pm.rules
-		sudo wget -P /etc/udev/rules.d https://raw.githubusercontent.com/mahatmus-tech/arch-auto-install/refs/heads/main/files/89-nvidia-pm.rules	 
-		# mkinitcpio.conf
-	    sudo sed -i -E "s|^MODULES=.*|MODULES=( nvidia nvidia_modeset nvidia_uvm nvidia_drm )|" /etc/mkinitcpio.conf
-    fi
-
-    status "Setting fstrim ..."
-	# Get root filesystem type
-	local root_fs_type=$(findmnt -n -o FSTYPE /)
-	# Get the base device name (strip /dev/ and partition suffix)
-	local root_source=$(findmnt -n -o SOURCE /)
-	local root_device=$(basename "$root_source" | sed -E 's/p?[0-9]+$//')
-	
-	# Check for SSD or NVMe (rotational = 0)
-	local is_ssd_or_nvme="false"
-	if [[ -e /sys/block/$root_device/queue/rotational ]]; then
-	    if [[ "$(cat /sys/block/$root_device/queue/rotational)" == "0" ]]; then
-		is_ssd_or_nvme="true"
-	    fi
-	fi
-	
-	# Filesystems known to support TRIM
-	if [[ "$is_ssd_or_nvme" == "true" && \
-	      "$root_fs_type" =~ ^(ext3|ext4|btrfs|f2fs|xfs|vfat|exfat|jfs|nilfs2|ntfs-3g)$ ]]; then
-	    status "Filesystem '$root_fs_type' supports TRIM. Enabling fstrim.timer..."
-	    systemctl enable fstrim.timer
-	    sudo systemctl enable --now fstrim.timer    
-	fi
-
- 	if [[ "$is_ssd_or_nvme" == "true" ]]; then
-	    status "Setting ext4 root partition performance..."    
-	    # set async journal
-	    sudo tune2fs -E mount_opts=journal_async_commit $(findmnt -n -o SOURCE /)
-	    sudo tune2fs -o journal_data_writeback $(findmnt -n -o SOURCE /)
-	    # Define the UUID of the partition (adaptar para escolhera  partição)
-	    UUID=$(blkid -s UUID -o value $(findmnt -n -o SOURCE /))
-	    # Define the new mount options
-	    NEW_MOUNT_OPTIONS="defaults,noatime"
-	    # Edit the fstab file to change the mount options
-	    sudo sed -i -E "s|^UUID=$UUID.*|UUID=$UUID \/ ext4 $NEW_MOUNT_OPTIONS 0 2|" /etc/fstab
-	    # remount the root partition
-		if ! sudo mount -o remount /; then
-		    error "Failed to remount root partition."
-		fi
-    fi
-
-    # services    
+ 	fi 
 }
 
 # ======================
@@ -260,11 +185,8 @@ configure_hyprland() {
 main() {
 	exec > >(tee -a "$LOG_FILE") 2>&1
  
-	echo -e "\n${GREEN}🚀 Starting Arch Auto Install ${NC}"
+	echo -e "\n${GREEN}🚀 Starting Hyprland Install ${NC}"
 	
-	# Detection phase
-	detect_system
-
     if ! command -v dialog &> /dev/null; then
         echo -e "${YELLOW}Installing dialog for menu interface...${NC}"
         install_packages dialog
@@ -284,13 +206,12 @@ main() {
         case $selection in
             1)  install_hyprland ;;
             2)  install_jakoolit ;;
-            3)  configure_system ;;
+            3)  configure_hyprland ;;
         esac
     done
 	
 	echo -e "\n${GREEN}✅ Installation completed successfully!${NC}"
 	echo -e "${YELLOW}Please reboot your system to apply all changes.${NC}"
-	echo -e "Consider copying your dotfiles to ~/.config"
 }
 
 # Execute
